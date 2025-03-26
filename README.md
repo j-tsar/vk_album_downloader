1087f27fbfadddde3d824179f3b32b768a978392
j-tsar <7.CooL_Le3@proton.me>
2025-03-26 07:44:28 +0300

    fixes and new features introduced
    - fixed parsing of the albums with whitespaces after the last valid symbol;
    - service albums (profile, wall, saved photos) support added;
    - argument parser added - see README;
    - opt-in for exporting albums' metadata into .csv implemented;
    - opt-in for writing the script run into .log-file instead;

--- README.md
+++ README.md
@@ -1,30 +1,48 @@
 # vk_album_downloader
-Python script to download list of albums (including albums from private communities) from [vk.com](https://www.vk.com). Implemented with VK API
+Python script to download list of albums (including albums from private communities) from [vk.com](https://www.vk.com). Implemented using VK API.
 
 ## Installation ##
 In order to use this script you need to install [Python 3](https://www.python.org/downloads/).
 
-You also need additional **vk_api** module. Write next command to your command line / terminal after you have installed Python 3
+You also need additional **vk_api** module. Clone its repo from Github to your local storage (e.g. folder where you have cloned the downloader) and write in CMD:
 
-`$ pip install vk_api`
+`$ cd vk_api`
+`$ py setup.py install`
+
+**NOTE:** the pypi version of **vk_api** module doesn't work as of March 2025. Use the method provided above instead.
 
 ## Usage ##
-This script allows you to download:
-* Albums from user's profile
-* Albums from communities, including private communities
+```
+usage: VK Album Downloader [-h] [-u USER_DATA] [-a ALBUMS_LIST] [-o OUTPUT_FOLDER] [-m] [-l]
 
-**NOTE:** script yet can not download any special albums such as 'Saved images', 'Profile images', 'Images from wall'
+Python script for bulk downloading photo albums from VK.
+
+options:
+  -h, --help            show this help message and exit
+  -u USER_DATA, --user_data USER_DATA
+                        where the file with user data is (default: "data.txt")
+  -a ALBUMS_LIST, --albums_list ALBUMS_LIST
+                        path to text file with albums links (default: "albums_list.txt")
+  -o OUTPUT_FOLDER, --output_folder OUTPUT_FOLDER
+                        where to put downloaded albums (default: "vk_downloaded_albums")
+  -m, --export_metadata
+                        export albums' metadata (photos, comments, etc.) to CSV file
+  -l, --log             output a script run to .log file instead
+```
+
+This script allows you to download:
+* albums from user's page: profile photos, wall photos, saved photos;
+* albums from communities, including private ones.
 
 You have to create following files to gather input information:
-* *data.txt*. (Because of the *vk.com* privacy policy script needs to perform authentication before interacting with VK API. So put your login / phone number and password into this file)
-* *albums_list.txt* (Just put list of url to the albums that you want do download)
+* *data.txt*. (Because of the *vk.com* privacy policy script needs to perform authentication before interacting with VK API. So put your login / phone number and password into this file).
+* *albums_list.txt* (Just put list of url to the albums that you want do download).
 
-Script will automatically create directory **vk_downloaded_albums** to save albums to in the directory where the script is located
+Script will automatically create directory **vk_downloaded_albums** to save albums to in the directory where the script is located, if not specified.
 
-**NOTE:** all the file / directory names and paths can be modified in the script. There is also no need to put them in the directory with the script
+**NOTE:** all the file / directory names and paths can be specified in the script, via passed arguments.
 
 ## Examples of the input files ##
-
 ### data.txt ###
 File with user data:
 
--- vk_album_downloader.py
+++ vk_album_downloader.py
@@ -1,23 +1,21 @@
-import vk_api
-import requests
+import argparse
+import csv
+from datetime import datetime
 import os
 import re
-import datetime
+import requests
 import sys
+import vk_api
+
 
 def handler_captcha(captcha):
-    key = input(f'Enter captcha code {captcha.get_url()}: ').strip()
+    key = input(f'Enter captcha code: {captcha.get_url()}: ').strip()
     return captcha.try_again(key)
 
 
 def handler_2fa():
-    code = input(f'Enter 2fa code: ').strip()
+    code = input(f'Enter 2FA code: ').strip()
     return code, False
-
-
-path_to_downloaded_albums = 'vk_downloaded_albums'
-path_to_user_data = 'data.txt'
-path_to_albums_list = 'albums_list.txt'
 
 
 def print_progress(value, end_value, bar_length=20):
@@ -31,6 +29,9 @@ def print_progress(value, end_value, bar_length=20):
     sys.stdout.flush()
 
 
+SERVICE_IDS = {'0': '-6', '00': '-7', '000': '-15'}
+
+
 def process_url(url):
     verification = re.compile(r'^https://vk.com/album(-?[\d]+)_([\d]+)$')
     o = verification.match(url)
@@ -38,22 +39,25 @@ def process_url(url):
         raise ValueError('invalid album link: {}'.format(url))
     owner_id = o.group(1)
     album_id = o.group(2)
-    return {'owner_id': owner_id, 'album_id': album_id}
+    if album_id in SERVICE_IDS:
+        return {'owner_id': owner_id, 'album_id': SERVICE_IDS.get(o.group(2))}
+    else:
+        return {'owner_id': owner_id, 'album_id': album_id}
 
 
-def read_data():
+def read_data(path_to_user_data, path_to_albums_list):
     lines = []
     try:
         with open(path_to_user_data, 'r') as f:
             lines = [line.strip() for line in f if line.strip()]
     except FileNotFoundError as e:
         print(e)
-        print('please, fix the file name either in the folder or in the script')
+        print('please, fix the file name or either path to it')
         sys.exit(e.errno)
 
     if (lines.__len__() < 2):
-        print('unable to read login / phone number and password') 
-        print('please, check your user data in the file') 
+        print('unable to read user credentials')
+        print('please, check your user data in the file')
         sys.exit(1)
     l = lines[0]
     p = lines[1]
@@ -88,14 +92,61 @@ def download_image(url, local_file_name):
             file.write(chunk)
     return
 
+
+def gather_comments(api, o, a):
+    comments = []
+    temp = []
+    i = 0
+    while True:
+        temp += api.photos.getAllComments(owner_id=o, album_id=a, need_likes=1, offset=i, count=100)['items']
+        comments += temp
+        if len(temp) < 100:
+            break
+
+        temp.clear()
+        i += 100
+    return comments
+
+
 PAT_ILLEGAL_CHARS = re.compile(r'[/|:?<>*"\\]')
 
+
 def fix_illegal_album_title(title: str) -> str:
-    return PAT_ILLEGAL_CHARS.sub('_', title)
+    return PAT_ILLEGAL_CHARS.sub('_', title.rstrip())
 
 
 def main():
-    l, p, queries = read_data()
+    parser = argparse.ArgumentParser(
+                    prog='VK Album Downloader',
+                    description='Python script for bulk downloading'
+                    ' photo albums from VK.')
+    parser.add_argument('-u', '--user_data',
+                        help='where the file with user data is'
+                        ' (default: "data.txt")',
+                        default='data.txt')
+    parser.add_argument('-a', '--albums_list',
+                        help='path to text file with albums links'
+                        ' (default: "albums_list.txt")',
+                        default='albums_list.txt')
+    parser.add_argument('-o', '--output_folder',
+                    help='where to put downloaded albums'
+                    ' (default: "vk_downloaded_albums")',
+                    default='vk_downloaded_albums')
+    parser.add_argument('-m', '--export_metadata',
+                        help='export albums\' metadata (photos,'
+                        ' comments, etc.) to CSV file',
+                        action='store_true')
+    parser.add_argument('-l', '--log',
+                        help='output a script run to .log file instead',
+                        action='store_true')
+    args = parser.parse_args()
+
+    if args.log:
+        log_file = open("vk_album_downloader.log", "a")
+        sys.stdout = log_file
+        print("started " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+
+    l, p, queries = read_data(args.user_data, args.albums_list)
     vk_session = vk_api.VkApi(l, p, captcha_handler=handler_captcha, auth_handler=handler_2fa)
 
     try:
@@ -118,6 +169,7 @@ def main():
         try:
             album = api.photos.getAlbums(owner_id=o, album_ids=a)['items'][0]
             images_num = album['size']
+            album_title = "[{o_id}]-[{a_id}] ".format(o_id=o, a_id=a) + fix_illegal_album_title(album['title'])
 
             photos = []
 
@@ -126,11 +178,38 @@ def main():
         except vk_api.exceptions.ApiError as e:
             print('exception:')
             print(e)
-            return    
+            return
 
-        album_path = os.path.join(path_to_downloaded_albums, a)
+        album_path = os.path.join(args.output_folder, album_title)
         if not os.path.exists(album_path):
             os.makedirs(album_path)
+
+        if args.export_metadata:
+            metadata_album = open(os.path.join(album_path, 'album.csv'),
+                                    'w', newline='', encoding="utf-8-sig")
+            fieldnames_album = list(album.keys())
+            writer = csv.DictWriter(metadata_album, fieldnames=fieldnames_album)
+            writer.writeheader()
+            row_data = {key: value for key, value in zip(fieldnames_album, album.values())}
+            writer.writerow(row_data)
+            metadata_album.close()
+
+            metadata_comments = open(os.path.join(album_path, 'comments.csv'),
+                                     'w', newline='', encoding="utf-8-sig")
+            comments = gather_comments(api, o, a) if args.export_metadata else None
+            fieldnames_comments = list(comments[0].keys())
+            writer = csv.DictWriter(metadata_comments, fieldnames=fieldnames_comments)
+            writer.writeheader()
+            for i in range(len(comments)):
+                row_data = {key: value for key, value in zip(fieldnames_comments, comments[i].values())}
+                writer.writerow(row_data)
+            metadata_comments.close()
+
+            metadata_photos = open(os.path.join(album_path, 'photos.csv'),
+                                   'w', newline='', encoding="utf-8-sig")
+            fieldnames_photos = list(photos[0])
+            writer = csv.DictWriter(metadata_photos, fieldnames=fieldnames_photos)
+            writer.writeheader()
 
         print('downloading album: ' + a)
         cnt = 0
@@ -150,7 +229,17 @@ def main():
             download_image(largest_image_src, os.path.join(album_path, str(p['id']) + extension))
             cnt += 1
             print_progress(cnt, images_num)
+
+            if args.export_metadata:
+                row_data = {key: value for key, value in zip(fieldnames_photos, p.values())}
+                writer.writerow(row_data)
+        if args.export_metadata:
+            metadata_photos.close()
         print()
+    if args.log:
+        print("\nfinished " + datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+        print("======================================================")
+        log_file.close()
 
 
 if __name__ == "__main__":
